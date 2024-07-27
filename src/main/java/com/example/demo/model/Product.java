6de827aa3af2ef681bbf07cca921d8e80de81d34
Wladpx <wlademirpx@gmail.com>
2024-07-27 14:53:51 -0300

    Adiciona Dockerfile para deploy no Railway

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,5 +1,34 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    public Object getName() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+    public void setName(Object name2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setName'");
+    }
+    public Object getPrice() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+    }
+    public void setPrice(Object price2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPrice'");
+    }
+
+    // Getters and Setters
 }
--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
-public class ProductRepository {
+import com.example.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
